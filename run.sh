a853225ac1d1bfe862424f46d1943115da2e0239
Jay LaCroix <j@jlacroix.me>
2015-11-13 23:44:02 -0500

    WIP.

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cd /git/ansible && /usr/local/bin/ansible-playbook playbooks/automatic/main.yml
+cd /git/ansible && /usr/local/bin/ansible-playbook playbooks/automatic/main.yml > "/var/log/ansible/ansible-$(date +%Y-%m-%d_%H%M).log"
+
+find /var/log/ansible -type f -mtime +1 -delete
+
+rm -r /home/ansible/.ansible/tmp
