d7efc617c779c19b0b8dfa6078a230d837adf534
Jay LaCroix <j@jlacroix.me>
2015-12-23 11:27:58 -0500

    Renamed.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /git/ansible && /usr/local/bin/ansible-playbook playbooks/automatic/main.yml > "/var/log/ansible/ansible-$(date +%Y-%m-%d_%H%M).log"
+cd /git/ansible && /usr/local/bin/ansible-playbook playbooks/automatic/_site.yml > "/var/log/ansible/ansible-$(date +%Y-%m-%d_%H%M).log"
 
 find /var/log/ansible -type f -mtime +1 -delete
 
