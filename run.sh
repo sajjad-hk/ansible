47344ce09f4cc94f7fa49748cbdf089a7280d68b
Jay LaCroix <j@jlacroix.me>
2016-03-09 10:07:26 -0500

    Lowered retention period for log files.

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 CURRDATE=$(date +%Y-%m-%d_%H%M)
 
 # Run playbooks
-cd /git/ansible 
+cd /git/ansible
 
 git pull
 
 /usr/local/bin/ansible-playbook site.yml >> "/var/log/ansible/ansible-$CURRDATE.log"
 
-find /var/log/ansible -type f -mtime +1 -delete
+find /var/log/ansible -type f -mmin -360 -delete
 
 rm -r /home/ansible/.ansible
